826b96211e44bb9b2e0cd39881b83098ca7e910c
Josie Bigler <jbigler01@hamline.edu>
2021-07-07 14:40:35 -0500

    Add DB migration

---
+++ Ilm.CodeAudition.Service/Migrations/20210707193946_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ilm.CodeAudition.Service.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Timesheets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    ProjectName = table.Column<string>(nullable: true),
+                    Monday = table.Column<int>(nullable: false),
+                    Tuesday = table.Column<int>(nullable: false),
+                    Wednesday = table.Column<int>(nullable: false),
+                    Thursday = table.Column<int>(nullable: false),
+                    Friday = table.Column<int>(nullable: false),
+                    Total = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Timesheets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Timesheets");
+        }
+    }
+}
