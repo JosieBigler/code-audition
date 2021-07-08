2ceef0c9b0c08d29d325706d7b8f0964bf457a16
Josie Bigler <jbigler01@hamline.edu>
2021-07-08 17:01:41 -0500

    revert to sql lite as default

--- Ilm.CodeAudition.Service/TimesheetContext.cs
+++ Ilm.CodeAudition.Service/TimesheetContext.cs
@@ -19,7 +19,7 @@ namespace Ilm.CodeAudition.Service
         {
             if (!options.IsConfigured)
             {
-                options.UseSqlServer("Server=tcp:codeaudition.database.windows.net,1433;Initial Catalog=timesheet;Persist Security Info=False;User ID=hyeena;Password=Polta123Polta123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                options.UseSqlServer("Data Source=../Ilm.CodeAudition.Service/Ilm.CodeAudition.Service.db");
             }
         }
 
